9b8e13d1a294230640cc3e045f75a8e7453bced0
Avadh <avadh.vashisth@gmail.com>
2019-01-23 23:49:37 +0530

    BMI application code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { TabsPage } from '../pages/tabs/tabs';
+import { CalculatorPage } from  '../pages/calculator/calculator';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ClassField } from '@angular/compiler/src/output/output_ast';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    TabsPage
+    TabsPage,
+    CalculatorPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    TabsPage
+    TabsPage,
+    CalculatorPage
   ],
   providers: [
     StatusBar,
